566c0701dbdf061631521f14b0041231fd825e9a
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-05-30 15:01:58 -0300

    chore: estrutura inical do projeto

---
+++ src/main/java/com/sylviavitoria/blogpets/BlogpetsApplication.java
@@ -0,0 +1,13 @@
+package com.sylviavitoria.blogpets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sylviavitoria/blogpets/BlogpetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sylviavitoria.blogpets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
