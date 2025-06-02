e64c6c3bfeda7d835440d0c51235e7d84bbe3030
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-06-02 15:50:59 -0300

    feat: adicionar métodos para busca, listagem, atualização e exclusão de posts na interface IPost

--- src/main/java/com/sylviavitoria/blogpets/interfaces/IPost.java
+++ src/main/java/com/sylviavitoria/blogpets/interfaces/IPost.java
@@ -1,8 +1,14 @@
 package com.sylviavitoria.blogpets.interfaces;
 
+import org.springframework.data.domain.Page;
+
 import com.sylviavitoria.blogpets.dto.PostRequest;
 import com.sylviavitoria.blogpets.dto.PostResponse;
 
 public interface IPost {
     PostResponse criar(PostRequest postRequest);
+    PostResponse bucarPorId(Long id);
+    Page<PostResponse> listarTodos(int page, int size, String sort);
+    PostResponse atualizar(Long id, PostRequest postRequest);
+    void excluir(Long id);
 }
