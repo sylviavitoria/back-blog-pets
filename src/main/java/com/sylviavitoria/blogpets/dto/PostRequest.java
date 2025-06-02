ce2b0bd8393518b59ba6cdb3c5da8df804a365d4
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-06-02 15:50:43 -0300

    feat: adicionar anotações de schema para os campos na classe PostRequest e PostResponse

--- src/main/java/com/sylviavitoria/blogpets/dto/PostRequest.java
+++ src/main/java/com/sylviavitoria/blogpets/dto/PostRequest.java
@@ -1,5 +1,6 @@
 package com.sylviavitoria.blogpets.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -8,16 +9,18 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PostRequest {
-    private Long id;
-    
+
+    @Schema(description = "Titulo de um Post", example = "Como preparar sua casa para um novo pet")
     @NotBlank(message = "O título é obrigatório")
     @Size(min = 5, max = 100, message = "O título deve ter entre 5 e 100 caracteres")
     private String titulo;
-    
+
+    @Schema(description = "Descrição de um Post", example = "Vai receber um novo amigo peludo? Veja dicas fáceis para deixar sua casa segura e acolhedora para o seu pet desde o primeiro dia!")
     @NotBlank(message = "A descrição é obrigatória")
     @Size(min = 10, message = "A descrição deve ter pelo menos 10 caracteres")
     private String descricao;
-    
+
+    @Schema(description = "Autor de um Post", example = "Julia Silva")
     @NotBlank(message = "O autor é obrigatório")
     @Size(min = 3, message = "O nome do autor deve ter pelo menos 3 caracteres")
     private String autor;
--- src/main/java/com/sylviavitoria/blogpets/dto/PostResponse.java
+++ src/main/java/com/sylviavitoria/blogpets/dto/PostResponse.java
@@ -1,14 +1,21 @@
 package com.sylviavitoria.blogpets.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
+@Schema(description = "Dados de resposta de um Post")
 public class PostResponse {
 
+    @Schema(description = "Titulo de um Post")
     String titulo;
+    
+    @Schema(description = "Descrição de um Post")
     String descricao;
+
+    @Schema(description = "Autor de um Post")
     String autor;
 
 }
